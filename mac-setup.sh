ddf576c6bc90d92b32ef72f604eaf5c47ab6e103
Brandon Banks <b93banks@gmail.com>
2019-10-02 10:10:53 -0400

    use asdf to manage kubectl version and installing

--- mac-setup.sh
+++ mac-setup.sh
@@ -5,6 +5,11 @@ chmod 0700 $HOME/.ssh
 # installing packages
 brew update
 
+brew install \
+  coreutils automake autoconf openssl \
+  libyaml readline libxslt libtool unixodbc \
+  unzip curl
+
 xcode-select --install
 
 # git
@@ -14,8 +19,15 @@ git config --global user.email "b93banks@gmail.com"
 git config --global color.ui auto
 
 # kubernetes
-brew install kubernetes-cli
 brew cask install minikube
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+cd ~/.asdf
+git checkout "$(git describe --abbrev=0 --tags)"
+# reload shell
+zsh
+# install kubectl
+asdf plugin-add kubectl
+asdf install kubectl 1.14.1
 
 # python
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
