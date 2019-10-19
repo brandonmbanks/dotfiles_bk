401e1b96768a2a7d965c60964c6c7642e6de9611
Brandon Banks <b93banks@gmail.com>
2019-10-18 22:04:13 -0400

    fix script and install packages if needed

--- mac-setup.sh
+++ mac-setup.sh
@@ -1,9 +1,15 @@
 # ssh dir
-mkdir $HOME/.ssh
+mkdir -p $HOME/.ssh
 chmod 0700 $HOME/.ssh
 
 # install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    : # homebrew already installed
+fi
 
 # installing packages
 brew update
@@ -16,7 +22,7 @@ brew install \
 xcode-select --install
 
 # download antigen
-curl -L git.io/antigen > ~/antigen.zsh
+[ -f ~/antigen.zsh ] || curl -L git.io/antigen > ~/antigen.zsh
 
 # git
 brew install git
@@ -24,48 +30,50 @@ git config --global color.ui auto
 
 # kubernetes
 brew cask install minikube
-rm -rf ~/.asdf
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+# download asdf if not exists
+[ -d ~/.asdf ] || git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 cd ~/.asdf
 git checkout "$(git describe --abbrev=0 --tags)"
-if grep -Fq "asdf" ~/.zshrc
+cd -
+if [ $(cat ~/.zshrc | grep -c "asdf") -eq 0 ];
 then
-    # asdf sh completion already exists
+    echo '\n# asdf' >> ~/.zshrc
+    echo '. $HOME/.asdf/asdf.sh' >> ~/.zshrc
+    echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
+    source ~/.zshrc > /dev/null # reload shell
 else
-    echo -e '\n# asdf' >> ~/.zshrc
-    echo -e '. $HOME/.asdf/asdf.sh' >> ~/.zshrc
-    echo -e '. $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
-    zsh  # reload shell
+    : # asdf sh completion already exists
 fi
 # install kubectl
 asdf plugin-add kubectl
 asdf install kubectl 1.14.1
+touch ~/.tool-versions
+echo "kubectl 1.14.1" > ~/.tool-versions
 
 # node
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
-if grep -Fxq "nvm" ~/.zshrc
+# download nvm if not exists
+[ -d "~/.nvm" ] || curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
+if [ $(cat ~/.zshrc | grep -c "nvm") -eq 0 ];
 then
-    # nvm sh completion already exists
-else
-    echo -e '\n# nvm' >> ~/.zshrc
-    echo -e 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshrc
-    echo -e '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> ~/.zshrc
-    echo -e '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> ~/.zshrc
     zsh  # reload shell
+else
+    : # nvm sh completion already exists
 fi
 nvm install node
 
 # python
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-if grep -Fxq "pyenv" ~/.zshrc
+# download pyenv if not exists
+[ -d ~/.pyenv ] || git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+if [ $(cat ~/.zshrc | grep -c "pyenv") -eq 0 ];
 then
-    # pyenv sh completion already exists
-else
-    echo -e '\n# pyenv' >> ~/.zshrc
-    echo -e 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
-    echo -e 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-    echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
+    echo '\n# pyenv' >> ~/.zshrc
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+    echo 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
     zsh  # reload shell
+else
+    : # pyenv sh completion already exists
 fi
 pyenv install 3.7.3
+zsh # reload shell
 
